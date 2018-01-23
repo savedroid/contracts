5b428bca312ef46c1485a013ec4066cd6f46bba1
Miguel Vaz <miguel.vaz@d-fine.de>
2018-01-23 14:18:21 +0100

    typos

--- truffle.js
+++ truffle.js
@@ -14,7 +14,6 @@ module.exports = {
     rinkeby: {
       host: "localhost", // Connect to geth on the specified
       port: 8545,
-      from: "0x6b018f0b646f50a436e25d034624ed9b6a1d0bb7", // default address to use for any transaction Truffle makes during migrations
       network_id: 4,
       gas: 5612388 // Gas limit used for deploys
     }
